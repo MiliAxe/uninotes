1cab7f25fd5dd76cc67f7937d7bbd7cb1c7f81b3
MiliAxe <miliaxe0@gmail.com>
2024-02-15 16:13:08 +0330

    vault backup: 2024-02-15 16:13:08

--- Algorithms/codes/textsegmentation.py
+++ Algorithms/codes/textsegmentation.py
@@ -1,11 +1,26 @@
-import enchant
+# import re
+
+# file = open("/usr/share/hunspell/en_US.dic", "r")
+# words = re.sub("[^\w]", " ", file.read()).split()
+# file.close()
 
 
 def is_english_word(word: str):
-    dictionary = enchant.Dict("en_US")
-    return dictionary.check(word)
+    dictionary = {"Awesomeness", "Awesome", "very", "cool"}
+    return word in dictionary
 
 
-word = "hello"
+def is_sentence_english(sentence):
+    if sentence == "":
+        return True
+    for index, _ in enumerate(sentence):
+        if is_english_word(sentence[: index + 1]):
+            print(f"Found word! {sentence[:index + 1]}")
+            rest_english = is_sentence_english(sentence[index + 1 :])
+            if rest_english:
+                return True
+    return False
 
-print(is_english_word(word))
+
+print(is_sentence_english("Awesomenessverycooll"))
+# print("a" in words.words(words))
